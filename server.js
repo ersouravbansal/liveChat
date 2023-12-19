df810ec6d2d4461698c98cab5154c99c9682d3e3
Sourav <sourav@ndtvs-MacBook-Air.local>
2023-12-19 18:42:06 +0530

    creating livechat

---
+++ server.js
@@ -0,0 +1,55 @@
+let express = require('express');
+const socketio = require('socket.io');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
+const server = app.listen(port, () => {
+    console.log("server is running on port", server.address().port);
+});
+const io = socketio(server)
+io.on('connection', (socket) => {
+    console.log('New connection Socket IO')
+})
+const db_url = "mongodb+srv://ndtvlivechat:4Pcyva4dHHNCTxSI@chatapp.g12liiu.mongodb.net/messages?retryWrites=true&w=majority"
+mongoose.connect(db_url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("db connection successful");
+    })
+    .catch((err) => {
+        console.log("db connection failed", err);
+    });
+
+const MessageSchema = new mongoose.Schema({
+    name: String,
+    message: String
+});
+const Message = mongoose.model('Message', MessageSchema);
+
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// get: will get all the messages from the database
+app.get('/messages', async (req, res) => {
+    try {
+        const messages = await Message.find({});
+        res.send(messages);
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+});
+
+// post: will save new messages created by the user to the database
+app.post('/messages', async (req, res) => {
+    try {
+        let message = new Message(req.body);
+        await message.save();
+        io.emit('message', req.body);
+        res.sendStatus(200);
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+});
+app.use(express.static(__dirname));
