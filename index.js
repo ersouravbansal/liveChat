dc1a6b81eea5a98d6a2531193fbf4c535bac8e11
SOURAV <36538958+ersouravbansal@users.noreply.github.com>
2023-12-21 17:36:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,18 @@
-require('dotenv').config();
-const express = require('express');
+require('dotenv').config()
+let express = require('express');
 const socketio = require('socket.io');
 const mongoose = require('mongoose');
 const { loremIpsum } = require('lorem-ipsum');
 const app = express();
-const port = process.env.portNumber;
+const port = process.env.portNumber
 const server = app.listen(port, () => {
     console.log("server is running on port", server.address().port);
 });
-const io = socketio(server);
-
+const io = socketio(server)
 io.on('connection', (socket) => {
-    console.log('New connection Socket IO');
-
-    // Handle joining a chatroom
-    socket.on('join', (chatroom) => {
-      socket.join(chatroom);
-      console.log(`Socket ${socket.id} joined chatroom ${chatroom}`);
-    });
-
-    // Handle creating a new chatroom
-    socket.on('createRoom', (newRoomName) => {
-      // You can add logic to save the new room to the database if needed.
-      console.log(`New chatroom created: ${newRoomName}`);
-      
-      // Broadcast the new room to all connected clients
-      io.emit('newRoom', newRoomName);
-    });
-});
-
-const db_url = process.env.MongodbURI;
+    console.log('New connection Socket IO')
+})
+const db_url = process.env.MongodbURI
 mongoose.connect(db_url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("db connection successful");
@@ -49,14 +32,14 @@ mongoose.connect(db_url, { useNewUrlParser: true, useUnifiedTopology: true })
       MessageSchema.set('timestamps', true);
       const Message = mongoose.model('Message', MessageSchema);
 
-app.use(express.static(__dirname));
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.get('/messages/:chatroom', async (req, res) => {
+// get: will get all the messages from the database
+app.get('/messages', async (req, res) => {
     try {
-        const messages = await Message.find({ chatroom: req.params.chatroom });
+        const messages = await Message.find({});
         res.send(messages);
     } catch (err) {
         console.error(err);
@@ -64,11 +47,12 @@ app.get('/messages/:chatroom', async (req, res) => {
     }
 });
 
+// post: will save new messages created by the user to the database
 app.post('/messages', async (req, res) => {
     try {
         let message = new Message(req.body);
         await message.save();
-        io.to(req.body.chatroom).emit('message', req.body);
+        io.emit('message', req.body);
         res.sendStatus(200);
     } catch (err) {
         console.error(err);
@@ -98,11 +82,6 @@ app.get('/initialize', async (req, res) => {
         }
 
         res.sendStatus(200);
-
-app.get('/chatrooms', async (req, res) => {
-    try {
-        const chatrooms = await Message.distinct('chatroom');
-        res.send(chatrooms);
     } catch (err) {
         console.error(err);
         res.sendStatus(500);
